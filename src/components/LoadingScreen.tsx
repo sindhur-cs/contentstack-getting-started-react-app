5fd313bf599998ae2676d5a3ffc4d7331b2dca04
sindhur-cs <sindhur.shabaraya@contentstack.com>
2025-04-29 17:04:05 +0530

    change in images, favicon

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -5,8 +5,10 @@ const LoadingScreen = () => {
   return (
     <div className="loading-screen">
       <img
-        src="https://images.contentstack.io/v3/assets/bltb07d61d76cca54b3/blt24c3ecd25fc51bce/667d01ad1e1583fd023a7777/Logomark.png"
+        src="/drinkitup.png"
         alt="Logo"
+        height={80}
+        width={80}
       />
     </div>
   );
